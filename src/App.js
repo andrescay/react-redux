6aad5b7259513bedfc223d2fe78e33193f8439a5
hsk <cristiandres4444@hotmail.com>
2025-05-20 15:01:43 -0500

    featactions|reducers): implement reducer action SET_POKEMONS

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import { Col } from 'antd';
-import Searcher from './components/Searcher';
-import PokemonList from './components/PokemonList';
-import logo from './statics/logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
-import { getPokemon } from './api';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Col } from "antd";
+import Searcher from "./components/Searcher";
+import PokemonList from "./components/PokemonList";
+import { setPokemons } from "./actions";
+import logo from "./statics/logo.svg";
+import "./App.css";
+import { getPokemon } from "./api";
 
 function App() {
-  const [pokemons, setPokemons] = useState([]);
+  const pokemons = useSelector((state) => state.pokemons);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchPokemons = async () => {
       const pokemonsRes = await getPokemon();
-      setPokemons(pokemonsRes);
+      dispatch(setPokemons(pokemonsRes));
     };
 
     fetchPokemons();
-  }, []);
-
+  }, [dispatch]);
 
   return (
     <div className="App">
       <Col span={4} offset={10}>
         <img src={logo} alt="Pokedux" />
-      </Col> 
+      </Col>
       <Col span={8} offset={8}>
-        <Searcher/>
+        <Searcher />
       </Col>
       <Col>
-        <PokemonList pokemons={pokemons}/> 
+        <PokemonList pokemons={pokemons} />
       </Col>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { pokemonsReducer } from "./reducers/pokemons";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const store = createStore(
+  pokemonsReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
